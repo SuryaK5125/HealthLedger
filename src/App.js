a07373cd989d0fb9733eaf35666fdfef48c6cc34
Surya K <suryaadhi789@gmail.com>
2026-04-28 19:11:02 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-// pages
 import Dashboard from "./pages/Dashboard";
 import Profiles from "./pages/Profiles";
 import ProfileDetail from "./pages/ProfileDetail";
 import Appointments from "./pages/Appointments";
 
-// components you already have
 import UploadForm from "./components/UploadForm";
 import RecordsList from "./components/RecordsList";
 
@@ -22,25 +20,18 @@ function App() {
   return (
     <BrowserRouter>
       <div style={{ padding: "1rem 2rem", maxWidth: 1100, margin: "0 auto" }}>
-        
-        {/* Show navbar only if logged in */}
         {user && <Navbar />}
 
         <Routes>
-          {/* Home route */}
           <Route
             path="/"
-            element={
-              user ? <Navigate to="/dashboard" /> : <Login />
-            }
+            element={user ? <Navigate to="/dashboard" /> : <Login />}
           />
 
-          {/* Dashboard visible to all */}
           <Route path="/dashboard" element={<Dashboard />} />
-
           <Route path="/profiles" element={<Profiles />} />
+          <Route path="/profiles/:id" element={<ProfileDetail />} />
 
-          {/* Protected routes */}
           <Route
             path="/upload"
             element={user ? <UploadForm /> : <Navigate to="/" />}
@@ -56,11 +47,11 @@ function App() {
             element={user ? <Appointments /> : <Navigate to="/" />}
           />
 
-          {/* Explicit login route */}
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </BrowserRouter>
   );
 }
+
 export default App;
